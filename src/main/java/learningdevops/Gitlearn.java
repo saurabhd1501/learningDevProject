607d79b01c25918ca3b729163d3352ef4e4a12d4
Saurabh D <saurabhd1501@gmail.com>
2025-02-16 17:29:34 +0530

    saurah clash with Mona

--- src/main/java/learningdevops/Gitlearn.java
+++ src/main/java/learningdevops/Gitlearn.java
@@ -3,12 +3,8 @@ package learningdevops;
 public class Gitlearn {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub  // Saurabh Dhiman
 		System.out.println("I am coming this in branch b2");
-<<<<<<< HEAD
-
-=======
->>>>>>> 2e93540da3b715483c33921121de97081e935e1a
 	}
 
 }
